8923cc0f10b3bf46e61241e6ef706a86dcfb40b6
Carlos Barrera <carlos.barreraacuna@gmail.com>
2025-02-21 16:55:12 -0500

    Conectando open ia

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,6 +1,8 @@
 import { createFlow } from '@builderbot/bot';
 import { mainFlow } from './mainFlow';  
+import { faqFlow } from './faqFlow';
 
 export default createFlow([
-    mainFlow
+    mainFlow,
+    faqFlow,
  ]);
--- src/templates/mainFlow.ts
+++ src/templates/mainFlow.ts
@@ -5,7 +5,7 @@ import { faqFlow } from './faqFlow'
 const mainFlow = addKeyword(EVENTS.WELCOME)
     // .addAnswer(`🙌 Hola este es patito IA*`)
     .addAction(async (ctx, ctxFn) => {
-        return ctxFn.gotoFlow('faqFlow');
+        return ctxFn.gotoFlow(faqFlow)
     })
 
 export { mainFlow};
